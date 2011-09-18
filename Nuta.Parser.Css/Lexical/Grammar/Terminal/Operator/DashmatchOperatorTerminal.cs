1ee7ea23ca394ab2252fbf83489ecc8e4cd5f280
Oleg Shuruev <shuruev@gmail.com>
2011-09-19 01:31:11 +0400

    Minor update.

---
+++ Nuta.Parser.Css/Lexical/Grammar/Terminal/Operator/DashmatchOperatorTerminal.cs
@@ -0,0 +1,14 @@
+﻿using Nuta.Parser.Lexical;
+
+namespace Nuta.Parser.Css.Lexical
+{
+	public class DashmatchOperatorTerminal : LexicalItem
+	{
+		public static readonly DashmatchOperatorTerminal S = new DashmatchOperatorTerminal();
+
+		public override bool Parse(LexicalState state)
+		{
+			return ParseWord(state, "|=");
+		}
+	}
+}
---
+++ Nuta.Parser.Css/Lexical/Grammar/Terminal/Operator/IncludesOperatorTerminal.cs
@@ -0,0 +1,14 @@
+﻿using Nuta.Parser.Lexical;
+
+namespace Nuta.Parser.Css.Lexical
+{
+	public class IncludesOperatorTerminal : LexicalItem
+	{
+		public static readonly IncludesOperatorTerminal S = new IncludesOperatorTerminal();
+
+		public override bool Parse(LexicalState state)
+		{
+			return ParseWord(state, "~=");
+		}
+	}
+}
