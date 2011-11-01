e419efab55c39194a2f2dca45ce4acfffb6caf39
Oleg Shuruev <shuruev@gmail.com>
2011-11-01 19:04:48 +0400

    Added a huge number of W3C test suites. Thanks to pbelousov!

--- NutaParser/Class2.cs
+++ NutaParser/Class2.cs
@@ -2,6 +2,11 @@ namespace A
 {
 	public class Class2
 	{
+		public string selectedTargetOU
+		{
+			get { return "aa"; }
+		}
+
 		public string this[long x, short y, int z]
 		{
 			get { return null; }
